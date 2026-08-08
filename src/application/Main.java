0fe82bc80b9cbbd7dc0919bbcba58fe5a18b17f0
Guilherme Menegotto <gmhoeller1@gmail.com>
2026-08-08 00:06:11 -0300

    Seller entity class

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,12 +1,16 @@
 package application;
 
 import model.entities.Department;
+import model.entities.Seller;
+
+import java.time.LocalDate;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
         Department obj = new Department(1, "Books");
-        System.out.println(obj);
+        Seller sl = new Seller(1, "João", "Joao@gmail.com", LocalDate.now(), 300.0, obj);
+        System.out.println(sl);
     }
 }
